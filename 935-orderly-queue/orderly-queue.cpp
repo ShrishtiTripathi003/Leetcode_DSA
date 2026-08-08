3a3bf20b00a3404415c4261e694e4b597308d013
Shrishti Tripathi <shrishti.tripathi@s.amity.edu>
2026-08-08 19:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (13.41%) - LeetSync

---
+++ 935-orderly-queue/orderly-queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k>1)
+        {
+            sort(s.begin(),s.end());
+            return s;
+
+        }
+        string result=s;
+        for(int i=0;i<s.size();i++)
+        {
+            string temp=s.substr(i)+s.substr(0,i);
+            result=min(result,temp);
+        }
+        return result;
+    }
+};
